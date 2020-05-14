6f19bd39ca5b446ad9a2475773099ef662c54149
issachareli <59080214+issachareli@users.noreply.github.com>
2020-05-14 15:19:50 +0900

    Update Home.js

--- src/views/pages/sponsor/Home.js
+++ src/views/pages/sponsor/Home.js
@@ -1,44 +1,30 @@
 import React, { Component } from 'react';
 
-import WPAPI from '../../../layouts/wpClient';
-import FrontpageItem from '../../../layouts/FrontpageItem';
-
-class Home extends Component {
-
-  constructor () {
-    super();
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      books: []
-    }
- 
+      posts: []
+    };
   }
-
-  componentDidMount (){
-
-    const bookUrl = WPAPI.books;
-
-    fetch(bookUrl)
-    .then(response => response.json())
-    .then(response => {
-      this.setState({
-        books: response
+  componentDidMount() {
+    axios
+      .get(
+        "https://home.mymoneylife.co/wp-json/wp/v2/pages/132"
+      )
+      .then(res => {
+        this.setState({ posts: res.data.posts });
+        console.log(this.state.posts);
       })
-    })
+      .catch(error => console.log(error));
   }
- 
+
   render() {
-
-    
     return (
-
-      <div className="container">
-        
-          <FrontpageItem posts={this.state.books} />
-         
+      <div className="blog">
+        <h1 className="sectionTitle">Articles</h1>
+        {this.state.posts.map(post => <ArticlePreview post={post} />)}
       </div>
-
     );
   }
 }
-
-export default Home;
