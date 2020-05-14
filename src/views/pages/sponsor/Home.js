fed3db828009d24ef3587dbd41bd57d8ce98c8dd
issachareli <59080214+issachareli@users.noreply.github.com>
2020-05-14 16:49:10 +0900

    Update Home.js

--- src/views/pages/sponsor/Home.js
+++ src/views/pages/sponsor/Home.js
@@ -1,32 +1,44 @@
-import React, { Component } from 'react';
-import ArticlePreview from '../../../layouts/ArticlePreview';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-export default class Home extends Component {
+class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       posts: []
     };
+    this.createMarkup = this.createMarkup.bind();
   }
+
   componentDidMount() {
-    axios
-      .get(
-        "https://home.mymoneylife.co/wp-json/wp/v2/pages/132"
-      )
-      .then(res => {
-        this.setState({ posts: res.data.posts });
-        console.log(this.state.posts);
-      })
-      .catch(error => console.log(error));
+    axios.get("https://home.mymoneylife.co/wp-json/wp/v2/pages/132").then(posts => {
+      this.setState({
+        posts: posts.data
+      });
+    });
+  }
+
+  createMarkup(html) {
+    return { __html: html };
   }
 
   render() {
     return (
-      <div className="blog">
-        <h1 className="sectionTitle">Articles</h1>
-        {this.state.posts.map(post => <ArticlePreview post={post} />)}
+      <div>
+        {this.state.posts.map(post => (
+          <Link to={`/${post.slug}`} key={post.id}>
+            <div className="card" key={post.id}>
+              <div className="card-content">
+                <h3>{post.title.rendered}</h3>
+                <div dangerouslySetInnerHTML={this.createMarkup( post.excerpt.rendered )} />
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     );
   }
 }
+
+export default Home;
