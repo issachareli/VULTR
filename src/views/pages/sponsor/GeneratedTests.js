e94c05b8b3d192e5e372dae0d4a9283165098ffc
issachareli <59080214+issachareli@users.noreply.github.com>
2020-05-23 07:43:53 +0900

    Update GeneratedTests.js

--- src/views/pages/sponsor/GeneratedTests.js
+++ src/views/pages/sponsor/GeneratedTests.js
@@ -1,80 +1,85 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   Card,
   CardBody,
   Table,
-  Button, CardHeader, Input, Col,
-} from 'reactstrap';
-import axios from 'axios';
-import QRCode from 'qrcode';
-import config from '../../../config/env';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import '../../../assets/style/style.css';
-import '../../../assets/style/Dashboard.css';
-import { Modal } from 'react-bootstrap';
+  Button, CardHeader, Input, Col
+} from 'reactstrap'
+import axios from 'axios'
+import QRCode from 'qrcode'
+import config from '../../../config/env'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import '../../../assets/style/style.css'
+import '../../../assets/style/Dashboard.css'
+import { Modal } from 'react-bootstrap'
 
 class GeneratedTest extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.state = {
-      generatedTests: [], 
-      addModalIsOpen:false,
-      minusModalIsOpen:false,
-      deleteModalIsOpen:false,
+      generatedTests: [],
+      addModalIsOpen: false,
+      minusModalIsOpen: false,
+      deleteModalIsOpen: false,
       qrCodeModalIsOpen: false,
-      selectedLink:null,
-      selectedCredits:null,
-      credits:null,
-      qrCodeLink: "", 
-      weChatAlert: false,
-    };
+      selectedLink: null,
+      selectedCredits: null,
+      credits: null,
+      qrCodeLink: '',
+      weChatAlert: false
+    }
 
-    this.qrCodeRef = React.createRef();
+    this.qrCodeRef = React.createRef()
 
-    this.openAddModal = this.openAddModal.bind(this);
-    this.openMinusModal = this.openMinusModal.bind(this);
-    this.openDeleteModal = this.openDeleteModal.bind(this);
-    this.openQRCodeModal = this.openQRCodeModal.bind(this);
-    this.closeQRCodeModal = this.closeQRCodeModal.bind(this);
-    this.closeAddModal = this.closeAddModal.bind(this);
-    this.closeMinusModal = this.closeMinusModal.bind(this);
-    this.addCreditRequest = this.addCreditRequest.bind(this);
-    this.deleteLinkRequest = this.deleteLinkRequest.bind(this);
-    this.closeDeleteModal = this.closeDeleteModal.bind(this);
-    this.generateQRCode = this.generateQRCode.bind(this);
-  }
-  openAddModal() {
-    this.setState({addModalIsOpen: true,deleteModalIsOpen:false});
-  }
-  openMinusModal() {
-    this.setState({minusModalIsOpen: true,minusModalIsOpen:false});
-  }
-  openDeleteModal() {
-    this.setState({deleteModalIsOpen: true,addModalIsOpen:false});
+    this.openAddModal = this.openAddModal.bind(this)
+    this.openMinusModal = this.openMinusModal.bind(this)
+    this.openDeleteModal = this.openDeleteModal.bind(this)
+    this.openQRCodeModal = this.openQRCodeModal.bind(this)
+    this.closeQRCodeModal = this.closeQRCodeModal.bind(this)
+    this.closeAddModal = this.closeAddModal.bind(this)
+    this.closeMinusModal = this.closeMinusModal.bind(this)
+    this.addCreditRequest = this.addCreditRequest.bind(this)
+    this.deleteLinkRequest = this.deleteLinkRequest.bind(this)
+    this.closeDeleteModal = this.closeDeleteModal.bind(this)
+    this.generateQRCode = this.generateQRCode.bind(this)
   }
 
-  openQRCodeModal() {
-    this.setState({qrCodeModalIsOpen: true});
+  openAddModal () {
+    this.setState({ addModalIsOpen: true, deleteModalIsOpen: false })
   }
 
-  closeQRCodeModal() {
-    this.setState({qrCodeModalIsOpen: false, qrCodeLink: ""});
+  openMinusModal () {
+    this.setState({ minusModalIsOpen: true, minusModalIsOpen: false })
   }
 
-  closeAddModal() {
-    this.setState({credit:null});
-    this.setState({addModalIsOpen: false});
-    this.componentDidMount();
+  openDeleteModal () {
+    this.setState({ deleteModalIsOpen: true, addModalIsOpen: false })
   }
-   closeMinusModal() {
-    this.setState({credit:null});
-    this.setState({minusModalIsOpen: false});
-    this.componentDidMount();
+
+  openQRCodeModal () {
+    this.setState({ qrCodeModalIsOpen: true })
   }
-  closeDeleteModal(){
-    this.setState({deleteModalIsOpen:false});
-    this.componentDidMount();
+
+  closeQRCodeModal () {
+    this.setState({ qrCodeModalIsOpen: false, qrCodeLink: '' })
+  }
+
+  closeAddModal () {
+    this.setState({ credit: null })
+    this.setState({ addModalIsOpen: false })
+    this.componentDidMount()
+  }
+
+  closeMinusModal () {
+    this.setState({ credit: null })
+    this.setState({ minusModalIsOpen: false })
+    this.componentDidMount()
+  }
+
+  closeDeleteModal () {
+    this.setState({ deleteModalIsOpen: false })
+    this.componentDidMount()
   }
 
   sendWhatsappMessage = (url) => {
@@ -82,125 +87,129 @@ class GeneratedTest extends Component {
   }
 
   sendWechatMessage = () => {
-    window.open('https://web.wechat.com/');
-    this.closeWeChatAlert();
+    window.open('https://web.wechat.com/')
+    this.closeWeChatAlert()
   };
 
   openWeChatAlert = () => {
-    this.setState({weChatAlert: true});
+    this.setState({ weChatAlert: true })
   }
 
   closeWeChatAlert = () => {
-    this.setState({weChatAlert: false});
+    this.setState({ weChatAlert: false })
   }
 
-  shouldComponentUpdate(nextProps, nextState, nextContext) {
+  shouldComponentUpdate (nextProps, nextState, nextContext) {
     return this.state.credits === nextState.credits
   }
 
-  componentDidMount() {
-    const token = localStorage.getItem('Token');
+  componentDidMount () {
+    const token = localStorage.getItem('Token')
     axios.get(`${config.url}/api/v1/sponsor/generated-tests`, { headers: { Authorization: `Bearer ${token}` } })
       .then(res => {
-        this.setState({ generatedTests: res.data.payload });
+        this.setState({ generatedTests: res.data.payload })
       }).catch(err => {
-      console.log(err);
-    });
+        console.log(err)
+      })
   }
 
   handleClick = (id) => {
-    document.querySelector(`#copy-url-${id}`).style.display = 'revert';
-    setTimeout(function() {
-      document.querySelector(`#copy-url-${id}`).style.display = 'none';
-    }, 2000);
+    document.querySelector(`#copy-url-${id}`).style.display = 'revert'
+    setTimeout(function () {
+      document.querySelector(`#copy-url-${id}`).style.display = 'none'
+    }, 2000)
   };
-  addCreditRequest(){
-    const token = localStorage.getItem('Token');
-    axios.post(`${config.url}/api/v1/sponsor/links/add-credit`,{link_uuid:this.state.selectedLink,credits:this.state.credits}, { headers: { Authorization: `Bearer ${token}` } })
-      .then(res => {
-        this.setState({credits:null,selectedLink:null,selectedCredits:null});
-        this.closeAddModal();
-      }).catch(err => {
-      console.log(err);
-      this.closeAddModal()
-    });
 
-      MinusCreditRequest(){
-    const token = localStorage.getItem('Token');
-    axios.post(`${config.url}/api/v1/sponsor/links/minus-credit`,{link_uuid:this.state.selectedLink,credits:this.state.credits}, { headers: { Authorization: `Bearer ${token}` } })
+  addCreditRequest () {
+    const token = localStorage.getItem('Token')
+    axios.post(`${config.url}/api/v1/sponsor/links/add-credit`, { link_uuid: this.state.selectedLink, credits: this.state.credits }, { headers: { Authorization: `Bearer ${token}` } })
       .then(res => {
-        this.setState({credits:null,selectedLink:null,selectedCredits:null});
-        this.closeAddModal();
+        this.setState({ credits: null, selectedLink: null, selectedCredits: null })
+        this.closeAddModal()
       }).catch(err => {
-      console.log(err);
-      this.closeAddModal()
-    });
-    
+        console.log(err)
+        this.closeAddModal()
+      })
   }
 
-  deleteLinkRequest(){
-    const token = localStorage.getItem('Token');
-    axios.post(`${config.url}/api/v1/sponsor/links/delete`,{link_uuid:this.state.selectedLink}, { headers: { Authorization: `Bearer ${token}` } })
+  minusCreditRequest () {
+    const token = localStorage.getItem('Token')
+    axios.post(`${config.url}/api/v1/sponsor/links/minus-credit`, { link_uuid: this.state.selectedLink, credits: this.state.credits }, { headers: { Authorization: `Bearer ${token}` } })
       .then(res => {
-        this.setState({selectedLink:null});
-        this.closeDeleteModal();
+        this.setState({ credits: null, selectedLink: null, selectedCredits: null })
+        this.closeMinusModal()
       }).catch(err => {
-      console.log(err);
-      this.closeDeleteModal()
-    });
-  }
-  handleMinusClick(record){
-    this.setState({selectedLink:record.uuid,selectedCredits:record.credits});
-    this.openMinusModal();
-  }
-  handlePlusClick(record){
-    this.setState({selectedLink:record.uuid,selectedCredits:record.credits});
-    this.openAddModal();
-  }
-  handleDeleteClick(record){
-    this.setState({selectedLink:record.uuid});
-    this.openDeleteModal();
+        console.log(err)
+        this.closeMinusModal()
+      })
   }
 
-  componentDidUpdate() {
-    if (this.qrCodeRef.current !== null && this.state.qrCodeLink !== "") {
+  deleteLinkRequest () {
+    const token = localStorage.getItem('Token')
+    axios.post(`${config.url}/api/v1/sponsor/links/delete`, { link_uuid: this.state.selectedLink }, { headers: { Authorization: `Bearer ${token}` } })
+      .then(res => {
+        this.setState({ selectedLink: null })
+        this.closeDeleteModal()
+      }).catch(err => {
+        console.log(err)
+        this.closeDeleteModal()
+      })
+  }
+
+  handleMinusClick (record) {
+    this.setState({ selectedLink: record.uuid, selectedCredits: record.credits })
+    this.openMinusModal()
+  }
+
+  handlePlusClick (record) {
+    this.setState({ selectedLink: record.uuid, selectedCredits: record.credits })
+    this.openAddModal()
+  }
+
+  handleDeleteClick (record) {
+    this.setState({ selectedLink: record.uuid })
+    this.openDeleteModal()
+  }
+
+  componentDidUpdate () {
+    if (this.qrCodeRef.current !== null && this.state.qrCodeLink !== '') {
       QRCode.toCanvas(this.state.qrCodeLink, { errorCorrectionLevel: 'H', margin: 1, width: 200 }, (err, canvas) => {
         if (err) {
-          console.log(err);
+          console.log(err)
         } else {
-          this.qrCodeRef.current.appendChild(canvas);
+          this.qrCodeRef.current.appendChild(canvas)
         }
-      });
+      })
     }
   }
 
-  generateQRCode(link) {
-    this.openQRCodeModal();
-    this.setState({qrCodeLink: link});
+  generateQRCode (link) {
+    this.openQRCodeModal()
+    this.setState({ qrCodeLink: link })
   }
 
-  render() {
-    const langaugeMap = {en:'English',cn:'Mandarin'};
+  render () {
+    const langaugeMap = { en: 'English', cn: 'Mandarin' }
     const GeneratedTestsURL = () => {
       if (this.state.generatedTests) {
         return this.state.generatedTests.map((record, index) => {
           return (
             <tr key={index}>
-             <td style={{ width: '20%' }}>
+              <td style={{ width: '20%' }}>
                 <span className="responsive">{record.campaign}</span>
               </td>
               <td style={{ width: '30%' }}>
                 <span className="responsive">{`${config.url}/credit/${record.uuid}/test`}</span>
-               {/* <CopyToClipboard text={`${config.url}/credit/${record.uuid}/test`}>
-                  <i className="fa fa-weixin pull-right" style={{ color: 'green' }} onClick={this.openWeChatAlert}/> 
+                {/* <CopyToClipboard text={`${config.url}/credit/${record.uuid}/test`}>
+                  <i className="fa fa-weixin pull-right" style={{ color: 'green' }} onClick={this.openWeChatAlert}/>
                 </CopyToClipboard>
                 <i onClick={() => this.sendWhatsappMessage(`${config.url}/credit/${record.uuid}/test`)} style={{ color: 'green' }} className="fa fa-whatsapp my-float mr-3 pull-right"/> */}
               </td>
-              <td style={{width:'10%'}}>{langaugeMap[record.language]}</td>
+              <td style={{ width: '10%' }}>{langaugeMap[record.language]}</td>
               <td style={{ width: '15%' }}><span className={'responsive mr-3'}>{record.credits}</span>
-                <span><i style={{fontSize:'1.5em',verticalAlign:'bottom'}} className="fa mx-3 float-right fa-trash" onClick={this.handleDeleteClick.bind(this,record)}/>
-                <i onClick={this.handlePlusClick.bind(this,record)} style={{fontSize:'1.5em',verticalAlign:'bottom'}} className="fa ml-3 float-right fa-plus-circle"/>
-          <i onClick={this.handleMinusClick.bind(this,record)} style={{fontSize:'1.5em',verticalAlign:'bottom'}} className="fa ml-3 float-right fa-minus-circle"/></span>
+                <span><i style={{ fontSize: '1.5em', verticalAlign: 'bottom' }} className="fa mx-3 float-right fa-trash" onClick={this.handleDeleteClick.bind(this, record)}/>
+                  <i onClick={this.handlePlusClick.bind(this, record)} style={{ fontSize: '1.5em', verticalAlign: 'bottom' }} className="fa ml-3 float-right fa-plus-circle"/>
+                  <i onClick={this.handleMinusClick.bind(this, record)} style={{ fontSize: '1.5em', verticalAlign: 'bottom' }} className="fa ml-3 float-right fa-minus-circle"/></span>
               </td>
               <td style={{ width: '25%' }}>
                 <CopyToClipboard text={`${config.url}/credit/${record.uuid}/test`}>
@@ -209,36 +218,35 @@ class GeneratedTest extends Component {
                   </Button>
                 </CopyToClipboard>
                 <Button size="md" color="primary" className="pull-left" onClick={() => this.generateQRCode(`${config.url}/credit/${record.uuid}/test`)}>
-                    <span className="responsive">QR Code</span>
+                  <span className="responsive">QR Code</span>
                 </Button>
                 <p id={`copy-url-${index}`} style={{ margin: '0px', marginTop: '5px', display: 'none' }}><span
                   className="responsive" style={{ marginLeft: '20px', color: 'green' }}>Link Copied!</span></p>
               </td>
             </tr>
-          );
-        });
-      } else
-        return (<tr></tr>);
-    };
+          )
+        })
+      } else { return (<tr></tr>) }
+    }
 
     return (
       <Card>
         <CardBody style={{ overflowX: 'auto' }}>
           <Table className="responsive-table">
             <thead>
-            <tr>
-              <th>Campaign</th>
-              <th>Link</th>
-              <th>Language</th>
-              <th>Credits</th>
-              <th></th>
-            </tr>
+              <tr>
+                <th>Campaign</th>
+                <th>Link</th>
+                <th>Language</th>
+                <th>Credits</th>
+                <th></th>
+              </tr>
             </thead>
             <tbody>
-            <GeneratedTestsURL/>
+              <GeneratedTestsURL/>
             </tbody>
           </Table>
-         <Modal show={this.state.weChatAlert} onHide={this.closeWeChatAlert}> 
+          <Modal show={this.state.weChatAlert} onHide={this.closeWeChatAlert}>
             <Modal.Header closeButton>
               <Modal.Title>Share test link</Modal.Title>
             </Modal.Header>
@@ -263,9 +271,9 @@ class GeneratedTest extends Component {
               <Input
                 type="number"
                 value={this.state.credits}
-                onChange={e => this.setState({ credits : parseInt(e.target.value)})}
+                onChange={e => this.setState({ credits: parseInt(e.target.value) })}
                 placeholder="Number of Credits"
-                style={{marginBottom: '5px'}}
+                style={{ marginBottom: '5px' }}
               />
             </Modal.Body>
             <Modal.Footer>
@@ -300,8 +308,8 @@ class GeneratedTest extends Component {
           </Modal>
         </CardBody>
       </Card>
-    );
+    )
   }
 }
 
-export default GeneratedTest;
+export default GeneratedTest
